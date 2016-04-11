b0e0576686f3630133a83e2301c5bb83395d695b
rs222kn <richard.soderman@gmail.com>
2016-04-11 11:22:05 +0200

    Started on socket connection

--- app.js
+++ app.js
@@ -3,26 +3,35 @@ const myMock = require('./myMock.js'); // TODO: remove when not needed
 
 const userDAL = require('./models/DAL/dbHelper.js');
 const express = require('express');
+const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const path = require('path');
+const jwt    = require('jsonwebtoken');
+const config = require('./config/config.js');
+
 
 const app = express();
 const port = 3334;
 userDAL();
 
+app.set('superSecret', config.secret);
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(morgan('dev'));
 
+//app.use('/api', apiRoutes);
 app.use('/', require('./routes/user.js'));
+app.use('/', require('./routes/authenticate.js'));
 
 app.use(function(req, res, next) {
-  res.status(404).send('404');
+    res.status(404).send('404');
 });
 
 app.use(function(err, req, res, next) {
-  console.error(err.stack);
-  res.status(500).send('500');
+    console.error(err.stack);
+    res.status(500).send('500');
 });
 
 app.all('/', function(req, res, next) {
@@ -31,6 +40,30 @@ app.all('/', function(req, res, next) {
     next();
 });
 
-const server = app.listen(port, function() {
-    console.log('Listening on port %d', server.address().port);
+const io = require('socket.io').listen(app.listen(port, function(){
+    console.log('Listening on port ', port);
+}), { log: false, origins: '*:*' });
+
+const lobby = require('./socketRoutes/lobby.js');
+
+io.sockets.on('connection', function (socket) {
+
+    socket.on('room', function(data) {
+        switch (data.room) {
+            case 'lobby':
+                    lobby.onLobbyJoin(data);
+                    socket.join(data.room);
+                break;
+            default:
+                return;
+        }
+    });
+
+    //socket.on('main', require('./socketRoutes/main.js'));
+
+    //socket.on('lobby', );
 });
+
+/*const sendLobbyMessage = () => {
+    io.sockets.in('lobby').emit();
+};*/
--- routes/user.js
+++ routes/user.js
@@ -3,41 +3,41 @@
 const router = require('express').Router();
 const db = require('../models/DAL/dbDAL.js');
 
+const colors = require('colors');
+
+// create new user
 router.route('/user/create')
     .post((req, res) => {
-        let bodyStr = '';
-        req.on('data', (chunk) => { // quick fix, body-parser wont work
-            bodyStr += chunk.toString();
-            const userData = JSON.parse(bodyStr);
 
-            if(!userData.hasOwnProperty('fbId') ||
-                !userData.hasOwnProperty('fbProfileImg') ||
-                !userData.hasOwnProperty('firstName') ||
-                !userData.hasOwnProperty('lastName')){
-                    res.status(400).send('400');
-            }
+        if(!req.body.hasOwnProperty('fbId') ||
+            !req.body.hasOwnProperty('fbProfileImg') ||
+            !req.body.hasOwnProperty('firstName') ||
+            !req.body.hasOwnProperty('lastName')){
+                res.status(400).send({ status: 'failed', message: 'invalid props' });
+        }
 
-            db.createNewPlayerWithCard(
-                userData.fbId,
-                userData.fbProfileImg,
-                userData.firstName,
-                userData.lastName)
-                .then(result => {
-                    console.log(result); // TODO: remove test log
-                    res.json(result);
-                })
-                .catch(e => res.status(500).send('500'));
-        });
+        db.createNewPlayerWithCard(
+            req.body.fbId,
+            req.body.fbProfileImg,
+            req.body.firstName,
+            req.body.lastName)
+            .then(result => {
+                console.log('created new user'.green);
+                console.log(result); // TODO: remove test log
+                res.json(result);
+            })
+            .catch(e => res.status(500).send('500'));
     });
 
-
+// get user
 router.route('/user/:fbid')
     .get((req, res) => {
         db.getUserByFbId(req.params.fbid)
-            .then(result => res.json(result))
+            .then(result => {console.log(result); res.json(result); })
             .catch(e => res.status(404).send('404'));
     });
 
+// get user cards
 router.route('/user/cards/:fbid')
     .get((req, res) => {
         db.getCardsByCreatorId(req.params.fbid)
@@ -47,5 +47,17 @@ router.route('/user/cards/:fbid')
             })
             .catch(e => res.status(404).send('404'));
     });
+
+
+// remove this later
+router.route('/users')
+    .get((req, res) => {
+        db.getAllUsers()
+            .then(result => {
+                res.json(result);
+            })
+            .catch(e => res.status(404).send('404'));
+    });
+
 
 module.exports = router;
