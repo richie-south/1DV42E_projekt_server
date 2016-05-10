01bd28767b430453163656099e48fc5af9187560
rs222kn <richard.soderman@gmail.com>
2016-05-10 11:35:39 +0200

    fixed bug, "removing card not done async"
    And added comments

--- models/DAL/dbLobby.js
+++ models/DAL/dbLobby.js
@@ -3,23 +3,37 @@
 const Lobby = require('./lobby.js');
 const co = require('co');
 
+/**
+ * [removes specifik card by id in lobby]
+ * @param  {[string]} id [id of a card]
+ * @return {[promise]}    [success or fail]
+ */
 const removeCard = (id) => {
     return new Promise(function(resolve, reject) {
         Lobby.findOne({ 'card._id': id })
             .exec()
-            .then((a) => {
-                a.remove();
-                resolve(true);
-            });
+            .then(a => a.remove())
+            .then(value => resolve(value))
+            .catch(e => reject(e));
     });
 };
 
+/**
+ * [creats a new lobby card]
+ * @param  {[object]} card [card instanse of mongoose schema Card]
+ * @return {[object]}      [new instanse of lobby]
+ */
 const createNew = (card) => {
     return new Lobby({
         card
     });
 };
 
+/**
+ * [adds card to lobbu]
+ * @param  {[object]} card [object of a card that has an instanse of mongoose schema Card]
+ * @return {[promise]}      [succsess or fail]
+ */
 const addCard = (card) => {
     return new Promise(function(resolve, reject) {
         const l = createNew(card);
@@ -29,6 +43,10 @@ const addCard = (card) => {
     });
 };
 
+/**
+ * [returns all card i lobby]
+ * @return {[promise]} [resolve all cards]
+ */
 const getAllCards = () => {
     return new Promise((resolve, reject) => {
         Lobby
@@ -38,10 +56,11 @@ const getAllCards = () => {
     });
 };
 
-const getSpecificCard = () => {
-    // TODO: implement this function
-};
-
+/**
+ * [finds specifik card in lobby by id]
+ * @param  {[string]} id [id of a card in lobby]
+ * @return {[object]}    [object of card in lobby]
+ */
 const getCardById = (id) => {
     return Lobby.findOne({ 'card._id': id}).exec();
 };
@@ -50,5 +69,5 @@ module.exports = {
     removeCard,
     addCard,
     getAllCards,
-    getSpecificCard
+    getCardById
 };
