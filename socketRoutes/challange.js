af60fdfa48e0a2c9df984e8b2fa381cdb0635c03
rs222kn <rs222kn@student.lnu.se>
2016-05-15 22:12:22 +0200

    started to implement challange socket route

--- socketRoutes/challange.js
+++ socketRoutes/challange.js
@@ -1,7 +1,5 @@
 'use strict';
 const db = require('../models/DAL/dbDAL.js');
-const dbUser = require('../models/DAL/dbUser.js');
-const dbCard = require('../models/DAL/dbCard.js');
 const co = require('co');
 
 const Challange = class {
@@ -9,8 +7,70 @@ const Challange = class {
         this.io = io;
     }
 
-    newGame(challangerCardId, opponentCardId){
+    /**
+     * [sends message to specific socket client]
+     * @param  {[string]} socketId [id of client]
+     * @param  {[string]} roomId   [id of room]
+     * @param  {[object]} socket   [socket object]
+     */
+    sendMessageToPlayer(socketId, roomId, socket){
+        socket.broadcast.to('/#'+socketId).emit('newGame', {
+            roomId
+        });
+    }
 
+    /**
+     * [crates a new game]
+     * @param  {[string]} challangerCardId   [id of a card]
+     * @param  {[string]} opponentCardId     [id of a card]
+     * @param  {[string]} challangeUserFbId  [facebook id of user]
+     * @param  {[object]} socket             [socket objet]
+     * @return {[promise]}                    [resolves to id of new game]
+     */
+    newGame(challangerCardId, opponentCardId, challangeUserFbId, socket){
+        return new Promise((resolve, reject) => {
+            co(function* (){
+                const [challangerCard, opponentCard, opponentSocketId] = yield [
+                    db.dbCard.getCardByCardId(challangerCardId),
+                    db.dbCard.getCardByCardId(opponentCardId),
+                    db.Lobby.getCardById(opponentCardId)
+                ];
+
+                const [challanger, opponent] = yield [
+                    db.dbUser.getUserByFbId(challangeUserFbId),
+                    db.getUserByCardId(opponentCard._id)
+                ];
+
+                const game = db.dbChallange.newChallange(
+                    challanger,
+                    challangerCard,
+                    opponent,
+                    opponentCard
+                );
+                yield game.save();
+                this.sendMessageToPlayer(opponentSocketId.card.socketId, game._id, socket);
+                return game._id;
+            }.bind(this))
+            .then(roomId => resolve(roomId))
+            .catch(e => reject(e));
+
+        });
+    }
+
+    /**
+     * [sends information about game]
+     * @param  {[string]} roomId [room to sent to]
+     * @param  {[type]} socket [socket object]
+     */
+    sendGameInfo(roomId, socket){
+        db.dbChallange.getChallangeById(roomId)
+            .then(challange => {
+                this.io.in(roomId).emit('gameInfo', {
+                    roomId,
+                    challange
+                });
+            })
+            .catch(e => console.log('error', e));
     }
 };
 
