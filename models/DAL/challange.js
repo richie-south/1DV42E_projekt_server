b24b142a443301871d8a0e360c7dc1245d3490d2
rs222kn <rs222kn@student.lnu.se>
2016-05-21 10:57:37 +0200

    added specific props to challenger and opponent

--- models/DAL/challange.js
+++ models/DAL/challange.js
@@ -3,19 +3,30 @@
 const mongoose = require('mongoose');
 
 const challangeSchema = mongoose.Schema({
-    props: {
-        healCards: { type: Number, min: 4, default: 4, required: true},
-        attackCards: { type: Number, min: 4, default: 4, required: true },
-        blockCards: { type: Number, min: 4, default: 4, required: true },
+    challanger: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    challangerCard: { type: mongoose.Schema.Types.ObjectId, ref: 'Card', required: true },
+    challangerRounds: [ { type: mongoose.Schema.Types.ObjectId, ref: 'GameRound' } ],
+
+    challangerProps: {
+        healCards: { type: Number, min: 0, default: 4, required: true},
+        attackCards: { type: Number, min: 0, default: 4, required: true },
+        blockCards: { type: Number, min: 0, default: 4, required: true },
         maxLife: { type: Number, min: 100, default: 100, required: true },
         life: { type: Number, min: 0, default: 100, required: true }
     },
 
-    challanger: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    challangerCard: { type: mongoose.Schema.Types.ObjectId, ref: 'Card', required: true },
-
+    // opponent
     opponent: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
     opponentCard: { type: mongoose.Schema.Types.ObjectId, ref: 'Card', required: true },
+    opponentRounds: [ { type: mongoose.Schema.Types.ObjectId, ref: 'GameRound' } ],
+
+    opponentProps: {
+        healCards: { type: Number, min: 0, default: 4, required: true},
+        attackCards: { type: Number, min: 0, default: 4, required: true },
+        blockCards: { type: Number, min: 0, default: 4, required: true },
+        maxLife: { type: Number, min: 100, default: 100, required: true },
+        life: { type: Number, min: 0, default: 100, required: true }
+    },
 
     createdAt: { type: Date, required: true, default: Date.now }
 });
