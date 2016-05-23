cfe167edc6c50f24d19490d197d2a6f5aca39acf
rs222kn <rs222kn@student.lnu.se>
2016-05-23 11:19:39 +0200

    replaced color

--- models/color.js
+++ models/color.js
@@ -2,7 +2,7 @@
 
 const color = class {
     constructor() {
-        this.colors = ['#FF5722', '#4CAF50', '#00BCD4', '#2196F3', '#E91E63', '#9C27B0'];
+        this.colors = ['#FF9800', '#4CAF50', '#00BCD4', '#2196F3', '#E91E63', '#9C27B0'];
     }
 
     getRandomColor(){
